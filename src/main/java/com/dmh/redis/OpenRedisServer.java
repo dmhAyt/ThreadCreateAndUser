b7976172c90749ec8eeac50d7121e791ac02b954
19527 <meihou.deng@hand-china.com>
2018-08-14 13:25:34 +0800

    pom.xml 删除了一些依赖，加入了 jedis 依赖（用于操作 redis）。添加了 Redis 的简单实用，开启本地的 Redis 服务器。

---
+++ src/main/java/com/dmh/redis/OpenRedisServer.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C), 2015-2018,
+ * FileName: OpenRedisServer
+ * Author:   deng_yt
+ * Date:     2018/8/14 11:19
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.dmh.redis;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ *
+ * @author deng_yt
+ * @create 2018/8/14
+ * @since 1.0.0
+ */
+public class OpenRedisServer {
+
+}
---
+++ src/main/java/com/dmh/redis/RedisOperate.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C), 2015-2018,
+ * FileName: RedisOperate
+ * Author:   deng_yt
+ * Date:     2018/8/14 9:55
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.dmh.redis;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ *
+ * @author deng_yt
+ * @create 2018/8/14
+ * @since 1.0.0
+ */
+public class RedisOperate {
+
+}
